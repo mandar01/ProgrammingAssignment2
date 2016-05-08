386eb6676887341df32d72d995c2a04fad066ead
mandar01 <mandar01@gmail.com>
2016-05-08 00:06:43 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+#to caching the inverse of a matrix rather than compute it repeatedly 
 
-## Write a short comment describing this function
+#assume that the matrix supplied is always invertible.
+
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
+}
+
+
+#this is just part of testing 
+testStub<-function ()
+{
+  set.seed(11109201)
+  r = rnorm(1000000)
+  mat = matrix(r, nrow=1000, ncol=1000)
+  
+  temp = makeCacheMatrix(mat)
+  
+  start = Sys.time()
+  mat1<-cacheSolve(temp)
+  timeTaken = Sys.time() - start 
+  #print(mat1)
+  print(timeTaken)
+  
+  
+  #second run for same matrix which shuld be faster 
+  start = Sys.time()
+  mat1<-cacheSolve(temp)
+  timeTaken = Sys.time() - start 
+  #print(mat1)
+  print(timeTaken)
+  
 }
